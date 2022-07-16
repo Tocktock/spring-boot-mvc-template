e28e239895c402131da990ecea1e8855a280aa6f
Park Ji Young <qnfmtm666@naver.com>
2022-07-16 21:09:11 +0900

    default 상태로 ~

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,15 +18,15 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+//	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	runtimeOnly("mysql:mysql-connector-java")
-	runtimeOnly("org.postgresql:postgresql")
+//	runtimeOnly("mysql:mysql-connector-java")
+//	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
