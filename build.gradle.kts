d19d7a8c248110cc26388a36fbd51b725099da51
ji-young <qnfmtm666@naver.com>
2022-07-17 21:04:24 +0900

    kotlin jpa 플러그인 적용, ktVersion 변수화

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	val ktVersion = "1.6.21"
 	id("org.springframework.boot") version "2.7.2-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+	id("org.jetbrains.kotlin.plugin.jpa") version ktVersion
+	kotlin("jvm") version ktVersion
+	kotlin("plugin.spring") version ktVersion
+
 }
 
 group = "tars.template"
