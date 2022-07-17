e4e186f0e955d67c92f98387e40f6f8b1d33b070
Park Ji Young <qnfmtm666@naver.com>
2022-07-17 21:32:40 +0900

    Users 에 인증관련 정보 추가

--- src/main/kotlin/tars/template/mvc/users/Users.kt
+++ src/main/kotlin/tars/template/mvc/users/Users.kt
@@ -2,6 +2,7 @@ package tars.template.mvc.users
 
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.security.crypto.bcrypt.BCrypt
 import java.time.LocalDateTime
 import javax.persistence.*
 
@@ -9,13 +10,22 @@ import javax.persistence.*
 @Table(name = "users")
 class Users(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long,
+    @Column(unique = true) val email: String,
+    password: String,
     @Enumerated(EnumType.STRING) val state: State,
     @CreatedDate val createdAt: LocalDateTime,
     @LastModifiedDate val updatedAt: LocalDateTime,
 ) {
+    var password = password
+        private set
 
     enum class State {
         ACTIVE,
         DEACTIVATED,
+    }
+
+    @PrePersist
+    fun hashingPassword() {
+        this.password = BCrypt.hashpw(this.password, BCrypt.gensalt())
     }
 }
