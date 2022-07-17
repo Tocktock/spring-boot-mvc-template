0632b27a43adecd79ab2ad915c81b6144fd38877
Park Ji Young <qnfmtm666@naver.com>
2022-07-17 23:05:34 +0900

    jwtFactory 적용

--- src/main/kotlin/tars/template/mvc/users/AuthService.kt
+++ src/main/kotlin/tars/template/mvc/users/AuthService.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service
 @Service
 class AuthService(
     private val usersRepository: UsersRepository,
+    private val jwtFactory: JwtFactory,
 ) {
     fun signUp(email: String, password: String): Users {
         return usersRepository.save(Users.create(email, password))
@@ -13,6 +14,10 @@ class AuthService(
     fun signIn(email: String, password: String): String {
         val user = usersRepository.findByEmail(email) ?: throw IllegalStateException("유저가 존재하지 않습니다.")
         val result = user.validatePassword(password)
-        return "hehe"
+        return if (result) {
+            jwtFactory.generate(mapOf(email to email))
+        } else {
+            throw IllegalStateException("아이디 또는 비밀번호를 확인해주세요.")
+        }
     }
 }
