67af970ec177b556ebb21e8b6c6afd4f324033d5
Park Ji Young <qnfmtm666@naver.com>
2022-07-17 22:44:01 +0900

    yaml 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 //	runtimeOnly("mysql:mysql-connector-java")
-//	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 
